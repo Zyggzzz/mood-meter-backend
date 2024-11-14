f091a2519357ff57d5baad29c09eae76c908d06a
Zyggzz <86673777+Zyggzzz@users.noreply.github.com>
2024-11-14 21:41:03 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ export const App = express();
 async function Main() {
   Index();
 
-  App.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  App.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
 
 export function Stop(res: Response, status: number, error: string) {
